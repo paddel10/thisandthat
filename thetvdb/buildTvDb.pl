4d2adec33bb3b2c9e2955d256a6b2b01865ae4e3
paddel10 <paddel10@hotmail.com>
2015-04-21 21:59:05 +0200

    destination folder for zip file

--- thetvdb/buildTvDb.pl
+++ thetvdb/buildTvDb.pl
@@ -8,15 +8,25 @@ use retrieveSeries;
 
 my $apiKey;
 my $seriesId;
+my $zipDest;
 my $help;
 
 GetOptions("apiKey=s" => \$apiKey,
            "seriesId=i" => \$seriesId,
+           "zipDest=s" => \$zipDest,
            "help|?|h" => \$help) or die("Error in command line arguments\n");
 
 help() if ($help or !$apiKey or !$seriesId);
 
-print "done\n";
+print "Retrieving serie id = " . $seriesId . " ... ";
+my $ret = retrieveSerieAsZip($seriesId, $apiKey, $zipDest);
+if (0 > $ret) {
+  print "failed (" . $ret . ")";
+} else {
+  print $ret;
+}
+
+print "\ndone\n";
 
 sub help {
   print <<HELP
@@ -28,9 +38,12 @@ retrieve the episodes from the serie.
 $0 [options]
 
 options:
-  -apiKey <tvDbAPIKey>    API key for accessing thetvdb.com content
-  -seriesId <seriesId>    Id of the serie as integer
-  -h|help|?               (optional) This page
+  -apiKey <tvDbAPIKey>      API key for accessing thetvdb.com content
+  -seriesId <seriesId>      Id of the serie as integer
+  -zipDest <path/filename>  (optional) path/filename with no suffix where to
+                            store the zip file. Current directory/seriesId is
+                            used if this option is omitted.
+  -h|help|?                 (optional) This page
 
 HELP
 ;
--- thetvdb/retrieveSeries.pm
+++ thetvdb/retrieveSeries.pm
@@ -12,8 +12,10 @@ $VERSION = 1.00;
 use warnings;
 use Data::Dumper;
 use File::Basename;
-use LWP::Simple qw($ua getstore);
+use File::Spec;
+use LWP::Simple qw($ua getstore is_error);
 use URI;
+use Cwd;
 
 #-----------------------------------------------------------------------
 # @functionName     retrieveSerieAsZip
@@ -21,11 +23,17 @@ use URI;
 # @param            serie id to retrieve
 # @param            api key to access thetvdb.com API
 # @param            path/filename with no suffix where to store the zip file
-# @return           return value of getstore() - check with is_error() or 
-#                   path/filename with .zip suffix where the zip file was stored 
+# @return           fail: negative error number of getstore(), success: path/filename
+#                   with .zip suffix where the zip file was stored 
 #-----------------------------------------------------------------------
 sub retrieveSerieAsZip {
     my($seriesId, $apiKey, $zipDest) = @_;
+    
+    if (!defined $zipDest) {
+      $zipDest = File::Spec->catfile(getcwd, $seriesId . ".zip");
+    } else {
+      $zipDest .= ".zip";
+    }
 
     my $patternUrl = "http://thetvdb.com/api/%s/series/%d/all/en.zip";
 
@@ -35,6 +43,7 @@ sub retrieveSerieAsZip {
 
     $ua->default_headers(HTTP::Headers->new(Accept => '*/*'));
     $ua->agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.54.16 (KHTML, like Gecko) Version/5.1.4 Safari/534.54.16");
-
-    return getstore($url, $zipDest . ".zip");
+    my $ret = getstore($url, $zipDest);
+    return (0-$ret) if (is_error($ret));
+    return $zipDest;
 }
