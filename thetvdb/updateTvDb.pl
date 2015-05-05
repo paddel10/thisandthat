999fa5d437b7227dfa50e97e513250d32897791f
patrickr <patrickr@i-web.ch>
2015-05-05 18:26:21 +0200

    initial updateTvDb.pl

---
+++ thetvdb/updateTvDb.pl
@@ -0,0 +1,10 @@
+# Get a list of all series and episode updates
+http://thetvdb.com/api/Updates.php?type=all&time=<previoustime>
+
+# Update each series in the update XML
+http://thetvdb.com/api/<api-key>/series/294332/en.xml
+
+# Update each episode in the update XML
+http://thetvdb.com/api/<api-key>/episodes/4239317/en.xml
+
+# Record <previoustime> for next update
